a07bcecf176fdeb784d9e1d40b737723d86fba78
PTMMMMM <2529640590@qq.com>
2019-08-07 22:24:06 +0800

    两张贝拉本体

--- expansions/script/c170030.lua
+++ expansions/script/c170030.lua
@@ -1,4 +1,114 @@
 --歌姬 伊莎贝拉•霍利
 function c170030.initial_effect(c)
-	
+	c:EnableReviveLimit()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(c170030.spcon)
+	e2:SetOperation(c170030.spop)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_CONTROL)
+	e3:SetDescription(aux.Stringid(170030,0))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,170030)
+	e3:SetTarget(c170030.controltg)
+	e3:SetOperation(c170030.controlop)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_IMMUNE_EFFECT)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(c170030.efilter)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_TODECK)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e5:SetTarget(c170030.rettg)
+	e5:SetOperation(c170030.retop)
+	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_TODECK)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_REMOVE)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetTarget(c170030.rettg)
+	e6:SetOperation(c170030.retop)
+	c:RegisterEffect(e6)
+end
+function c170030.efilter(e,te)
+	return te:IsActiveType(TYPE_SPELL+TYPE_TRAP) 
+end
+function c170030.spfilter(c,tp,g,sc)
+	return c:IsAbleToGraveAsCost() and Duel.GetLocationCountFromEx(tp)>0
+end
+function c170030.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(c170030.spfilter,tp,LOCATION_MZONE,0,nil)
+	if not c:IsAbleToGraveAsCost()  then
+		g:RemoveCard(c)
+	end
+	return  Duel.IsCanRemoveCounter(tp,1,0,0x10ff,6,REASON_COST) and g:CheckWithSumGreater(Card.GetLevel,6)
+end
+function c170030.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(c170030.spfilter,c:GetControler(),LOCATION_MZONE,0,nil)
+	if not c:IsAbleToGraveAsCost()  then
+		g:RemoveCard(c)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local sg=g:SelectWithSumGreater(tp,Card.GetLevel,6)
+	Duel.RemoveCounter(tp,1,0,0x10ff,6,REASON_COST)
+	Duel.SendtoGrave(sg,REASON_COST+REASON_DISCARD)
+end
+function c170030.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function c170030.retop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_EFFECT)
+	end
+end
+function c170030.filter(c)
+	return c:IsControlerCanBeChanged()
+end
+function c170030.controltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c170030.filter(chkc) end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE,1-tp,LOCATION_REASON_CONTROL)
+	if chk==0 then return ft>0 and Duel.IsExistingTarget(c170030.filter,tp,0,LOCATION_MZONE,1,nil) end
+	local ct=math.min(ft,2)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,c170030.filter,tp,0,LOCATION_MZONE,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,#g,0,0)
+end
+function c170030.controlop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
+	Duel.GetControl(tg,tp)
+	local tc=tg:GetFirst()
+	while tc do
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(1)
+	tc:RegisterEffect(e1)
+	tc=tg:GetNext()
+	end
 end
