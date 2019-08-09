ddbefcd2a38474c73761bc3adb792f1cf23ec00a
PTMMMMM <2529640590@qq.com>
2019-08-09 14:03:55 +0800

    change

--- expansions/script/c80700003.lua
+++ expansions/script/c80700003.lua
@@ -5,30 +5,20 @@ function c80700003.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c80700003.target)
-	e1:SetOperation(c80700003.activate)
-	c:RegisterEffect(e1)
-end
-function c80700003.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
-end
-function c80700003.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-	Duel.NegateRelatedChain(tc,RESET_TURN_SET)
-	local e1=Effect.CreateEffect(c)
-   -- e1:SetType(EFFECT_TYPE_ACTIVATE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetTarget(c80700003.eqtg)
 	e1:SetOperation(c80700003.eqop)
-	tc:RegisterEffect(e1)
-	end
+	c:RegisterEffect(e1)
+end
+function c80700003.filter(c)
+	return c:IsFaceup() 
+end
+function c80700003.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c80700003.filter(chkc) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(c80700003.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c80700003.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_HAND)
 end
 function c80700003.eqfilter(c,tc)
 	return c:IsLevelBelow(10)
@@ -36,49 +26,45 @@ function c80700003.eqfilter(c,tc)
 		and c:GetOriginalAttribute()==tc:GetOriginalAttribute()
 		and not c:IsForbidden() 
 end
-function c80700003.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(c80700003.eqfilter,tp,LOCATION_HAND,0,1,nil,tp,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_HAND)
-end
 function c80700003.eqop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c80700003.eqfilter),tp,LOCATION_HAND,0,1,1,nil,tc)
-	local tc=g:GetFirst()
-	if tc then
-		if not Duel.Equip(tp,tc,c,true) then return end
-		local e1=Effect.CreateEffect(c)
-		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+	local eq=Duel.SelectMatchingCard(tp,c80700003.eqfilter,tp,LOCATION_HAND,0,1,1,nil,tc)
+	local eqc=eq:GetFirst()
+	if eqc and Duel.Equip(tp,eqc,tc,true) then
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetValue(c80700003.eqlimit)
-		tc:RegisterEffect(e1)
-		local atk=tc:GetAttack()
-		local den=tc:GetDefense()
+		e1:SetLabelObject(tc)
+		eqc:RegisterEffect(e1)
+		local atk=eqc:GetAttack()
+		local den=eqc:GetDefense()
 		if atk>0 then
-			local e2=Effect.CreateEffect(c)
+			local e2=Effect.CreateEffect(e:GetHandler())
 			e2:SetType(EFFECT_TYPE_EQUIP)
 			e2:SetProperty(EFFECT_FLAG_OWNER_RELATE+EFFECT_FLAG_IGNORE_IMMUNE)
 			e2:SetCode(EFFECT_UPDATE_ATTACK)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e2:SetValue(atk)
-			tc:RegisterEffect(e2)
+			eqc:RegisterEffect(e2)
 		end
 		if den>0 then
-		   local e3=Effect.CreateEffect(c)
+		   local e3=Effect.CreateEffect(e:GetHandler())
 			e3:SetType(EFFECT_TYPE_EQUIP)
 			e3:SetProperty(EFFECT_FLAG_OWNER_RELATE+EFFECT_FLAG_IGNORE_IMMUNE)
 			e3:SetCode(EFFECT_UPDATE_DEFENSE)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e3:SetValue(den)
-			tc:RegisterEffect(e3) 
+			eqc:RegisterEffect(e3) 
 		end
 	end
 end
 function c80700003.eqlimit(e,c)
+	return c==e:GetLabelObject()
+end
+function c80700003.eqlimit2(e,c)
 	return e:GetOwner()==c
 end
