1421ad67fcdbb1704ff21ada8a093525a14ab591
PTMMMMM <2529640590@qq.com>
2019-08-13 21:28:56 +0800

    change_log

--- expansions/script/c76000030.lua
+++ expansions/script/c76000030.lua
@@ -1,18 +1,34 @@
 --Swan型神宫司·玉藻
 function c76000030.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcCodeRep(c,99999999,2,true,true)
+	aux.AddContactFusionProcedure(c,Card.IsAbleToGraveAsCost,LOCATION_HAND,0,Duel.SendtoGrave,REASON_COST)
+	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e1:SetCondition(c76000030.con)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c76000030.splimit)
 	c:RegisterEffect(e1) 
+	--indestructable
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetCountLimit(1)
-	e2:SetTarget(c76000030.setg)
-	e2:SetOperation(c76000030.seop)
-	c:RegisterEffect(e2)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetCondition(c76000030.con)
+	c:RegisterEffect(e2) 
+	--SearchCard
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c76000030.setg)
+	e3:SetOperation(c76000030.seop)
+	c:RegisterEffect(e3)
+end
+function c76000030.splimit(e,se,sp,st)
+	return bit.band(st,SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
 end
 function c76000030.con(e)
 	local f1=Duel.GetFieldCard(0,LOCATION_SZONE,5)
@@ -28,7 +44,7 @@ function c76000030.setg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c76000030.seop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c76000030.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c76000030.sefilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
