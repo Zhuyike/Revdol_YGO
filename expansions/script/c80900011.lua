51cd3ce5eb7cc1ef7dcf9bd05582c42f249221ca
zhuyike <zhuyike@bupt.edu.cn>
2019-08-14 00:32:17 +0800

    fix 玉藻沙滩闪耀五连拍

--- expansions/script/c80900011.lua
+++ expansions/script/c80900011.lua
@@ -2,39 +2,43 @@
 function c80900011.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1) 
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCondition(c80900011.spcon)
-	e2:SetTarget(c80900011.sptg)
-	e2:SetOperation(c80900011.spop)
-	c:RegisterEffect(e2)	
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCondition(c80900011.condition)
+	e1:SetTarget(c80900011.target)
+	e1:SetOperation(c80900011.operation)
+	c:RegisterEffect(e1)
 end
-function c80900011.cfilter(c) 
-	return  c:IsRace(RACE_CREATORGOD) and  c:GetPreviousLocation()==LOCATION_MZONE 
-	and c:GetPreviousControler()==tp
-end
-function c80900011.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c80900011.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c80900011.cfilter,1,nil,tp)
+end
+function c80900011.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c80900011.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c80900011.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_GRAVE)
+end
+function c80900011.cfilter(c,tp)
+	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE) and c:IsRace(RACE_CREATORGOD)
 end
 function c80900011.spfilter(c,e,tp)
 	return c:IsAttackBelow(2000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 	and c:IsRace(RACE_CREATORGOD)
 end
-function c80900011.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c80900011.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c80900011.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-end
-function c80900011.spop(e,tp,eg,ep,ev,re,r,rp)
+function c80900011.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(500)
+			tc:RegisterEffect(e1)
+			Duel.SpecialSummonComplete()
+		end
 	end
 end 
