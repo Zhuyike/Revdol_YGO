0e2966564b5ceee15101a60d6f85559228c21001
PTMMMMM <2529640590@qq.com>
2019-08-17 15:37:49 +0800

    add 搬砖小妹

--- expansions/script/c80900015.lua
+++ expansions/script/c80900015.lua
@@ -1,24 +1,16 @@
 --罗兹·冲浪·完美发挥
 function c80900015.initial_effect(c)
 	local e1=Effect.CreateEffect(c) 
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetTarget(c80900015.setg)
 	e1:SetOperation(c80900015.seop)
-	c:RegisterEffect(e1)	
-	local e2=Effect.CreateEffect(c) 
-	e2:SetCategory(CATEGORY_TOGRAVE)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetTarget(c80900015.destg)
-	e2:SetOperation(c80900015.desop)
-	c:RegisterEffect(e2)	
+	c:RegisterEffect(e1)	 
 end
 function c80900015.tgfilter1(c)
-	return c:IsFaceup() and c:IsRace(RACE_CREATORGOD) and c:IsAttackAbove(2000)
+	return c:IsFaceup() and c:IsRace(RACE_CREATORGOD)
 end
 function c80900015.sefilter1(c,e,tp)
 	return c:IsCode(99999999) 
@@ -29,38 +21,29 @@ function c80900015.sefilter2(c,e,tp)
 end
 function c80900015.setg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c80900015.tgfilter1(chkc) end
-	if chk==0 then return 
-	Duel.IsExistingTarget(c80900015.tgfilter1,tp,LOCATION_MZONE,0,1,nil) 
-	and Duel.IsExistingMatchingCard(c80900015.sefilter1,tp,LOCATION_DECK,0,1,nil,e,tp) 
-	and Duel.IsExistingMatchingCard(c80900015.sefilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c80900015.tgfilter1,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c80900015.tgfilter1,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK) 
+	local g=Duel.SelectTarget(tp,c80900015.tgfilter1,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,tp,LOCATION_MZONE)
 end
 function c80900015.seop(e,tp,eg,ep,ev,re,r,rp)
-		local c=e:GetHandler()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g1=Duel.SelectMatchingCard(tp,c80900015.sefilter1,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g2=Duel.SelectMatchingCard(tp,c80900015.sefilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		g1:Merge(g2)
-		Duel.SendtoHand(g1,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g1)
-end
-function c80900015.tgfilter2(c)
-	return c:IsFaceup() and c:IsRace(RACE_CREATORGOD) and c:IsAttackBelow(2000)
-end
-function c80900015.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c80900015.tgfilter2(chkc) end
-	if chk==0 then return 
-	Duel.IsExistingTarget(c80900015.tgfilter2,tp,LOCATION_MZONE,0,1,nil)  end
-	local g=Duel.SelectTarget(tp,c80900015.tgfilter2,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
-end
-function c80900015.desop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
-		Duel.SendtoGrave(tc,REASON_EFFECT)
+		if tc:GetAttack()>=2000 then
+			if Duel.IsExistingMatchingCard(c80900015.sefilter1,tp,LOCATION_DECK,0,1,nil,e,tp) and Duel.IsExistingMatchingCard(c80900015.sefilter2,tp,LOCATION_DECK,0,1,nil,e,tp) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+				local g1=Duel.SelectMatchingCard(tp,c80900015.sefilter1,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+				local g2=Duel.SelectMatchingCard(tp,c80900015.sefilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+				g1:Merge(g2)
+				Duel.SendtoHand(g1,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g1)
+			end
+		end
+		if tc:GetAttack()<=2000 then
+			Duel.SendtoGrave(tc,REASON_EFFECT)
+		end
 	end
 end
---
+++ expansions/script/c92700140.lua
@@ -0,0 +1,49 @@
+--搬砖小妹
+function c92700140.initial_effect(c)
+	--SearchCard
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON)
+	e1:SetTarget(c92700140.thtg1)
+	e1:SetOperation(c92700140.thop)
+	c:RegisterEffect(e1)
+	--SearchCard
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON)
+	e2:SetTarget(c92700140.thtg2)
+	e2:SetOperation(c92700140.thop)
+	c:RegisterEffect(e2)
+end
+function c92700140.filter1(c)
+	return c:IsSetCard(0xffff)  and c:IsAbleToHand()
+end
+function c92700140.thtg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_DECK) and c92700140.filter1(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c92700140.filter1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c92700140.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+
+function c92700140.filter2(c)
+	return (c:IsCode(92700150) or c:IsCode(92700160)) and c:IsAbleToHand()
+end
+function c92700140.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_DECK) and c92700140.filter2(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c92700140.filter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c92700140.filter2,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c92700140.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
