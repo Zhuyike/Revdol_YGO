8124e84d4f70ff9733f9c50e26543ffaaaffdd6a
zhuyike <zhuyike@bupt.edu.cn>
2019-09-13 02:56:31 +0800

    fix 爱老师 噩梦穹顶

--- expansions/script/c92700190.lua
+++ expansions/script/c92700190.lua
@@ -14,5 +14,17 @@ function c92700190.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	
+	--Atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetValue(c92700190.val)
+	c:RegisterEffect(e3)
+end
+function c92700190.val(e,c)
+	local count=Duel.GetMatchingGroupCount(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,0xff07)
+	if c:IsSetCard(0xff08) then return 400*count
+	else return 0 end
 end
