1cdb1fecc3a76a4fb0386ce4a5ec0db5a8d599fb
zhuyike <zhuyike@bupt.edu.cn>
2019-09-16 12:10:42 +0800

    fix 某A君

--- expansions/script/c170006.lua
+++ expansions/script/c170006.lua
@@ -1,12 +1,5 @@
 --某A君
 function c170006.initial_effect(c)
-	--cannot special summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.FALSE)
-	c:RegisterEffect(e1)
 	--summon effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(170006,0))
