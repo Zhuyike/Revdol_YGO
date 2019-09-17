97f6785b0b779167611f3b7e769e018348dfdcd6
zhuyike <zhuyike@bupt.edu.cn>
2019-09-18 00:34:07 +0800

    finish 伊贞鸭鸭

--- expansions/script/c60602000.lua
+++ expansions/script/c60602000.lua
@@ -1,121 +1,115 @@
 --伊贞 卡缇娅·乌拉诺娃
 function c60602000.initial_effect(c)
---
+	--fusion material
 	c:EnableReviveLimit()
+	aux.AddFusionProcCodeRep(c,99999999,3,false,true)
+	aux.AddContactFusionProcedure(c,Card.IsAbleToGraveAsCost,LOCATION_HAND,0,Duel.SendtoGrave,REASON_COST)
+	--SpecialSummon
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetDescription(aux.Stringid(60602000,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c60602000.e1con)
+	e1:SetTarget(c60602000.e1tg)
+	e1:SetOperation(c60602000.e1op)
 	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(LOCATION_EXTRA)
-	e2:SetCondition(c60602000.spcon)
-	e2:SetOperation(c60602000.spop)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_UPDATE_DEFENSE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e3:SetCondition(c60602000.dencon)
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_LIGHT))
-	e3:SetValue(1000)
-	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e4:SetCondition(c60602000.con)
-	e4:SetValue(1)
-	c:RegisterEffect(e4)
-	local e5=Effect.CreateEffect(c)
-	e5:SetCategory(CATEGORY_TODECK)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_TO_GRAVE)
-	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e5:SetTarget(c60602000.rettg)
-	e5:SetOperation(c60602000.retop)
-	c:RegisterEffect(e5)
-	local e6=Effect.CreateEffect(c)
-	e6:SetCategory(CATEGORY_TODECK)
-	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e6:SetCode(EVENT_REMOVE)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e6:SetTarget(c60602000.rettg)
-	e6:SetOperation(c60602000.retop)
-	c:RegisterEffect(e6)
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_IGNITION)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e7:SetCountLimit(1)
-	e7:SetTarget(c60602000.distg)
-	e7:SetOperation(c60602000.disop)
-	c:RegisterEffect(e7)
+	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(60602000,5))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c60602000.target)
+	e1:SetOperation(c60602000.operation)
+	c:RegisterEffect(e1)
 end
-function c60602000.cfilter(c)
-	return c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5
+function c60602000.filter(c)
+	return c:IsFaceup() and not c:IsDisabled()
 end
-function c60602000.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE)  end
-	if chk==0 then return Duel.IsExistingTarget(c60602000.cfilter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(60602000,2))
-	local g=Duel.SelectTarget(tp,c60602000.cfilter,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
-	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-	g:GetFirst():RegisterFlagEffect(c60602000,RESET_EVENT+RESETS_STANDARD+RESET_CHAIN,0,1,cid)
+function c60602000.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c60602000.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c60602000.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c60602000.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,g:GetFirst():GetControler(),LOCATION_MZONE)
 end
-function c60602000.disop(e,tp,eg,ep,ev,re,r,rp)
+function c60602000.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetReset(RESET_PHASE+PHASE_END,2)
-	tc:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_DISABLE_EFFECT)
-	e2:SetReset(RESET_PHASE+PHASE_END,2)
-	tc:RegisterEffect(e2)
-end
-function c60602000.dencon(e)
-	return e:GetHandler():IsPosition(POS_FACEUP_ATTACK)
-end
-function c60602000.con(e)
-	return e:GetHandler():IsPosition(POS_FACEUP_DEFENSE)
-end
-function c60602000.spfilter(c,tp,g,sc)
-	return c:IsAbleToGraveAsCost() and Duel.GetLocationCountFromEx(tp)>0
-end
-function c60602000.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetMatchingGroup(c60602000.spfilter,tp,LOCATION_MZONE,0,nil)
-	if not c:IsAbleToGraveAsCost()  then
-		g:RemoveCard(c)
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsType(TYPE_MONSTER) and not tc:IsDisabled() then
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
 	end
-	return  Duel.IsCanRemoveCounter(tp,1,0,0x10ff,6,REASON_COST) and g:CheckWithSumGreater(Card.GetLevel,6)
 end
-function c60602000.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetMatchingGroup(c60602000.spfilter,c:GetControler(),LOCATION_MZONE,0,nil)
-	if not c:IsAbleToGraveAsCost()  then
-		g:RemoveCard(c)
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	local sg=g:SelectWithSumGreater(tp,Card.GetLevel,6)
-	Duel.RemoveCounter(tp,1,0,0x10ff,6,REASON_COST)
-	Duel.SendtoGrave(sg,REASON_COST+REASON_DISCARD)
+
+function c60602000.e1con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_EXTRA)
 end
-function c60602000.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c60602000.e1tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,2,tp,LOCATION_DECK)
 end
-function c60602000.retop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_EFFECT)
+function c60602000.filter_yachi(c)
+	return c:IsSetCard(0xff01) and c:IsFaceup()
+end
+function c60602000.e1op(e,tp,eg,ep,ev,re,r,rp)
+	local count=Duel.GetMatchingGroupCount(c60602000.filter_yachi,tp,LOCATION_MZONE,0,nil)
+	local c=e:GetHandler()
+	if count>0 then
+		if Duel.SelectYesNo(tp,aux.Stringid(60602000,2)) then
+			--atk up
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(1000)
+			c:RegisterEffect(e1)
+		end
 	end
+	if count>1 then
+		if Duel.SelectYesNo(tp,aux.Stringid(60602000,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+			Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+	if count>2 then
+		if Duel.SelectYesNo(tp,aux.Stringid(60602000,3)) then
+			if Duel.IsPlayerCanDraw(tp,2) then
+				Duel.Draw(tp,2,REASON_EFFECT)
+			end
+		end
+	end
+	if count>3 then
+		if Duel.SelectYesNo(tp,aux.Stringid(60602000,4)) then
+			--immune
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_IMMUNE_EFFECT)
+			e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e2:SetRange(LOCATION_MZONE)
+			e2:SetValue(c60602000.efilter)
+			c:RegisterEffect(e2)
+		end
+	end
+end
+function c60602000.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
 end
