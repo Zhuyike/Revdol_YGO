f2d88a0c27979635ee458661b60601609ebcdabb
zhuyike <zhuyike@bupt.edu.cn>
2019-09-18 01:37:45 +0800

    fix a log, 剩余：愤怒的秃头，流枫渡人，三岁嗔顽，卡缇娅的神圣之羽，卡缇娅的最后绝唱，喀秋莎，审判律令，咸鸭掌

--- expansions/script/c60602004.lua
+++ expansions/script/c60602004.lua
@@ -2,8 +2,7 @@
 function c60602004.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1)
@@ -16,50 +15,20 @@ function c60602004.efcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
 function c60602004.cfilter(c)
-	return c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup()
 end
 function c60602004.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE)  end
-	if chk==0 then return Duel.IsExistingTarget(c60602004.cfilter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(60602004,2))
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup()  end
+	if chk==0 then return Duel.IsExistingTarget(c60602004.cfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,c60602004.cfilter,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
-	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-end
-function c60602004.subop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ct=c:GetTurnCounter()
-	ct=ct+1
-	c:SetTurnCounter(ct)
-	if (ct==2 or ct>2) then
-		Duel.Destroy(c,REASON_EFFECT)
-		c:ResetFlagEffect(1082946)
-	end
-end
-function c60602004.subcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetLabelObject()
-	local fid=e:GetLabel()
-	return c:GetFieldID()==fid
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,g:GetFirst():GetControler(),LOCATION_MZONE)
 end
 function c60602004.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(tc)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetCountLimit(1)
-		e1:SetRange(LOCATION_MZONE)
-		local fid=c:GetFieldID()
-		e1:SetLabel(fid)
-		e1:SetLabelObject(c)
-		e1:SetCondition(c60602004.subcon)
-		e1:SetOperation(c60602004.subop)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN+RESET_SELF_TURN,2)
-		tc:SetTurnCounter(0)
-		tc:RegisterEffect(e1)
-		tc:RegisterFlagEffect(1082946,RESET_PHASE+PHASE_END+RESET_OPPO_TURN+RESET_SELF_TURN,0,2)
-		c60602004[tc]=e1
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(1-tp) and tc:IsLocation(LOCATION_MZONE) then
+		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
--- expansions/script/c60602021.lua
+++ expansions/script/c60602021.lua
@@ -16,6 +16,35 @@ function c60602021.initial_effect(c)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetCondition(c60602021.spcon2)
 	c:RegisterEffect(e2)
+	--special summon from grave
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,60612021)
+	e3:SetCost(c60602021.spcost2)
+	e3:SetTarget(c60602021.sptg2)
+	e3:SetOperation(c60602021.spop2)
+	c:RegisterEffect(e3)
+end
+function c60602021.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+	end
+end
+function c60602021.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c60602021.filter_cost(c)
+	return c:IsAbleToRemoveAsCost() and c:IsType(TYPE_TRAP)
+end
+function c60602021.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c60602021.filter_cost,tp,LOCATION_HAND,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c60602021.filter_cost,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function c60602021.spcon1(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
