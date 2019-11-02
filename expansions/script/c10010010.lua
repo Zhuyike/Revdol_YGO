913d1d59380bd269975ba0b6029e84cbc7e1c100
zhuyike <zhuyike@bupt.edu.cn>
2019-11-03 02:06:48 +0800

    fix 小小风

--- expansions/script/c10010010.lua
+++ expansions/script/c10010010.lua
@@ -13,9 +13,9 @@ function c10010010.initial_effect(c)
 	--SearchCard
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetTarget(c10010010.setg)
 	e2:SetOperation(c10010010.seop)
 	c:RegisterEffect(e2)  
@@ -26,11 +26,11 @@ end
 function c10010010.fothfilter(c)
 	return  c:IsCode(60602008) and c:IsAbleToHand()
 end
-function c10010010.fothtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c10010010.setg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c10010010.fothfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c10010010.fothop(e,tp,eg,ep,ev,re,r,rp)
+function c10010010.seop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,c10010010.fothfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
