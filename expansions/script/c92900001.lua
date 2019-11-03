3a9f3fe5e51a19d57fa0708b8d1ac85accf65fef
xck233 <1124119259@qq.com>
2019-11-03 16:45:04 +0800

    add cdb;add 92900001

---
+++ expansions/script/c92900001.lua
@@ -0,0 +1,16 @@
+--伊贞型玉藻
+function c92900001.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,92900001)
+	e1:SetTarget(c92900001.tg)
+	e1:SetOperation(c92900001.op)
+	c:RegisterEffect(e1)
+
+	local e2=Effect.CreateEffect(c)
+	
+end
