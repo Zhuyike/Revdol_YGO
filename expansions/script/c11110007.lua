00f5c22a19b2a9656319f09c26554ed0f881ce98
yukari-Rohana <18917822382@163.com>
2019-11-05 02:12:05 +0800

    add 白发赤瞳控

---
+++ expansions/script/c11110007.lua
@@ -0,0 +1,29 @@
+--白发赤瞳控
+function c11110007.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,11110007)
+	e1:SetTarget(c11110007.tg)
+	e1:SetOperation(c11110007.op)
+	e1:SetDescription(aux.Stringid(11110007,0))
+	c:RegisterEffect(e1)  
+end
+function c11110007.filter(c)
+	return c:IsSetCard()==0xff00 and c:IsAbleToHand()
+end
+function c11110007.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetMatchingGroup(c11110007.filter,tp,LOCATION_DECK,0,1,nil)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c11110007.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c11110007.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		Duel.DiscardDeck(tp,1,REASON_EFFECT)
+	end
+end
