3b21fed6f225c7c6f49cbe840c62e01571763a74
zhuyike <zhuyike@bupt.edu.cn>
2019-11-06 01:35:22 +0800

    fix cdb; add 嘘嘘

---
+++ expansions/script/c11110002.lua
@@ -0,0 +1,60 @@
+--嘘嘘
+function c11110002.initial_effect(c)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(11110002,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,11110002)
+	e1:SetCost(c11110002.damcost)
+	e1:SetOperation(c11110002.damop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetDescription(aux.Stringid(11110002,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1,11110002+EFFECT_COUNT_CODE_DUEL)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetCost(c11110002.cost)
+	e2:SetTarget(c11110002.target)
+	e2:SetOperation(c11110002.operation)
+	c:RegisterEffect(e2)
+end
+function c11110002.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsLocation(LOCATION_REMOVED) then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+	end
+end
+function c11110002.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler())  and e:GetHandler():IsAbleToGrave() end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c11110002.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+end
+function c11110002.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemoveAsCost() end
+	Duel.Remove(c,POS_FACEUP,REASON_COST)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function c11110002.damop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.DiscardDeck(tp,1,REASON_EFFECT)>0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CHANGE_DAMAGE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(0)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
