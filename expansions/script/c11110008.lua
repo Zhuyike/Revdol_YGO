5f0f5971d94cc46acd2615c1a4618f71a0187cd0
xck233 <1124119259@qq.com>
2019-11-09 01:52:15 +0800

    add 11110008

---
+++ expansions/script/c11110008.lua
@@ -0,0 +1,35 @@
+--睡熊kuma
+function c11110008.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCondition(c11110008.condtion)
+	e1:SetOperation(c11110008.operation)
+	c:RegisterEffect(e1)
+
+end
+
+function c11110008.condtion(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
+		and c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetPreviousControler()==tp
+
+end
+
+function c11110008.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+	local gp=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,e:GetHandler())
+	Duel.ChangePosition(gp,POS_FACEUP_DEFENSE)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	Duel.BreakEffect()
+	Duel.DiscardDeck(tp,3,REASON_EFFECT)
+
+end
