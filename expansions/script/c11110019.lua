a44287a2e930ca1311564465f59f1fe7b32d4676
ApolloBoy <fengshun@allsworth.cn>
2019-11-13 21:51:53 +0800

    新增匿名lala

---
+++ expansions/script/c11110019.lua
@@ -0,0 +1,72 @@
+--匿名lala
+function c11110019.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(c11110019.actcona)
+	e1:SetOperation(c11110019.disop)
+	c:RegisterEffect(e1);
+end
+
+function c11110019.actcona(e)
+	local tp=e:GetHandler():GetControler()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0) <= Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+end
+
+function c11110019.spfilter(c,e,tp)
+	return c:IsSetCard(0xff00) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function c11110019.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.DiscardDeck(tp,5,REASON_DISCARD)
+	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=Duel.GetMatchingGroup(c11110019.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if ct<=0 or g:GetCount()<=0 then return end
+	ct=math.min(ct,g:GetCount())
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ct=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:SelectSubGroup(tp,aux.dncheck,false,ct,ct)
+	local tc=sg:GetFirst()
+	while tc do
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		tc:RegisterFlagEffect(11110019,RESET_EVENT+RESETS_STANDARD,0,1)
+		tc=sg:GetNext()
+	end
+	Duel.SpecialSummonComplete()
+	g:KeepAlive()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCountLimit(1)
+	e1:SetLabel(fid)
+	e1:SetLabelObject(g)
+	e1:SetCondition(c11110019.descon)
+	e1:SetOperation(c11110019.desop)
+	Duel.RegisterEffect(e1,tp)
+end
+
+
+function c11110019.desfilter(c,fid)
+	return c:GetFlagEffectLabel(11110019)==fid
+end
+
+function c11110019.descon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(c11110019.desfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function c11110019.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local tg=g:Filter(c11110019.desfilter,nil,e:GetLabel())
+	Duel.Destroy(tg,REASON_EFFECT)
+end
