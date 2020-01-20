45581412989f7d3ff2fc001720a8ea1e8ccfd77b
PTMMMMM <2529640590@qq.com>
2020-01-20 12:20:37 +0800

    add香冷金猊·唐玟,清墨糖厂·妙羽(未测试)

---
+++ expansions/script/c12220360.lua
@@ -0,0 +1,77 @@
+--香冷金猊·唐玟
+function c12220360.initial_effect(c)
+	--SpecialSummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c12220360.spcon)
+	e1:SetTarget(c12220360.sptg)
+	e1:SetOperation(c12220360.spop)
+	c:RegisterEffect(e1)   
+	--immune to des
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c12220360.indcon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)	
+	--Negate
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c12220360.condition)
+	e3:SetCost(c12220360.cost)
+	e3:SetTarget(c12220360.target)
+	e3:SetOperation(c12220360.operation)
+	c:RegisterEffect(e3)
+end
+function c12220360.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	return tc:IsControler(tp) and (tc:IsCode(12220370)) and tc:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function c12220360.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c12220360.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
+end
+function c12220360.indfilter(c)
+	return   c:IsCode(12220370)
+end
+function c12220360.indcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetMatchingGroupCount(c12220360.indfilter,tp,0,LOCATION_MZONE,1,nil)>0
+end
+function c12220360.condition(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
+	if re:IsHasCategory(CATEGORY_NEGATE)
+		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-tg:GetCount()>0
+end
+function c12220360.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),REASON_COST)
+end
+function c12220360.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c12220360.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+	e:GetHandler():RegisterFlagEffect(12220360,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
